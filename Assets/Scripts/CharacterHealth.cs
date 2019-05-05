a96de7aa52a285d041f4e7f022d76a6ea976058e
Philip Geske <pwg46@cornell.edu>
2019-05-05 17:29:55 -0400

    display health and game over when no health

---
+++ Assets/Scripts/CharacterHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterHealth : MonoBehaviour
+{
+    public int health;
+    public GameObject healthIndicator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthIndicator.GetComponent<UnityEngine.UI.Text>().text = "Health: " + health;
+
+    }
+
+    public void Damage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
