5c283082535d11228734a3906452f2524a4c1f24
Philip Geske <pwg46@cornell.edu>
2019-05-05 19:51:54 -0400

    added title screen

---
+++ Assets/Scripts/TitleInputHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleInputHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("here");
+        if (Input.GetKeyDown("return"))
+        {
+            Debug.Log("changing scene");
+            SceneManager.LoadScene("Play");
+        }   
+    }
+}
